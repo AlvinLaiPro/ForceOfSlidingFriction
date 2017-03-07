f662b3c2e6a8958d75f344863c6d3880701071c1
赖绿林 <123580@nd.com>
2017-03-07 15:33:51 +0800

    修改图片载入以便更好呈现

--- src/presenter/control/AppControl.js
+++ src/presenter/control/AppControl.js
@@ -60,6 +60,11 @@ export default class AppControl extends BaseControl {
         this.GraphControl = GraphControl.getInst().init(this);
         this.animate = AnimateControl.getInst().init(this);
         this.data = AppData.getInst().init(this, extendData);
+        
+        Promise.all(this.config.promiseArr).then(function(){
+           $(view).css('visibility','visible')
+            console.log('load complete');
+        });
         return this;
     }
 
--- src/presenter/model/AppConfig.js
+++ src/presenter/model/AppConfig.js
@@ -5,70 +5,92 @@
  */
 class AppConfig {
 
-	/**
-	 * 单例
-	 */
-	static _inst;
+    /**
+     * 单例
+     */
+    static _inst;
 
-	static getInst() {
-		AppConfig._inst = AppConfig._inst || new AppConfig();
-		return AppConfig._inst;
-	}
+    static getInst() {
+        AppConfig._inst = AppConfig._inst || new AppConfig();
+        return AppConfig._inst;
+    }
 
-	/**
-	 * 初始化
-	 */
-	init( app ) {
-		let that = this;
-		this.app = app;
-		this.fontSize = this.app.$view.find('#layout').css('font-size').replace(/px/,'');
-		this.basePath = './';
-		
-		this.animatePoint = null;
-		this.coefficient = [
-			{staticCoefficient: 0.6, dynamicCoefficient: 0.4},	// 木-木静摩擦因数与动摩擦因数
-			{staticCoefficient: 0.8, dynamicCoefficient: 0.6},  // 木-毛巾静摩擦因数与动摩擦因数
-			{staticCoefficient: 0.06, dynamicCoefficient: 0.04} // 木-玻璃静摩擦因数与动摩擦因数
-		];
+    /**
+     * 初始化
+     */
+    init(app) {
+        let that = this;
+        this.app = app;
+        this.fontSize = this.app.$view.find('#layout').css('font-size').replace(/px/, '');
+        this.basePath = './';
 
-		this.lineColor = {
-		[ that['coefficient'][0]['staticCoefficient']]: ['#eb8f35','#c48100', '#aa4801'],
-			[ that['coefficient'][1]['staticCoefficient']]: ['#0173a1','#2478dc','#244ddc'],
-			[ that['coefficient'][2]['staticCoefficient']]: ['#479600', '#038203', '#015c22']
-		}
-		//svg绘制参数
-		this.currentLineColor = '#FF0000';
-		this.currentLineWidth = 5*this.fontSize/24;
-		this.highlightLineColor = '#FF0000';
-		this.highlightLineWidth = 5*this.fontSize/24;
-		this.highlightPointRadius = 7.5*this.fontSize/24;
-		this.selectCircleColor = '#FFFC00';
-		this.selectedPointRadius = 12*this.fontSize/24;
+        this.animatePoint = null;
+        this.coefficient = [
+            { staticCoefficient: 0.6, dynamicCoefficient: 0.4 }, // 木-木静摩擦因数与动摩擦因数
+            { staticCoefficient: 0.8, dynamicCoefficient: 0.6 }, // 木-毛巾静摩擦因数与动摩擦因数
+            { staticCoefficient: 0.06, dynamicCoefficient: 0.04 } // 木-玻璃静摩擦因数与动摩擦因数
+        ];
 
-		this.MAX_LINE_NUM = 3;
-		
-		this.PULL_MAX = 5;  // 弹簧测力计最大拉力5N
-		this.WOOD_WEIGHT = 50;  // 木块重量
-		this.IRON_WEIGHT = 100;   // 砝码重量
-		this.g = 10;   // g = 10N/KG
-		this.STATIC_TIME = 3; // 动画总时间
-		this.CONVERSION_TIME = 0.2; // 动画总时间
-		this.DYNAMIC_TIME = 5.8; // 动画总时间
-		this.TOTAL_TIME = 9; // 动画总时间
-		this.WEIGHT_MAX = 2;
-		this.INIT_STATE =0;  //动画未开始状态
-		this.RUN_STATE =1;   //运动状态
-		this.PAUSE_STATE =2;  //暂停状态
-		this.COMPLETE_STATE =3;  //动画结束状态
-		this.SINGLE_SPEED =1;  //一倍速度
-		this.DOUBLE_SPEED =2;  //二倍速度
+        this.lineColor = {
+                [that['coefficient'][0]['staticCoefficient']]: ['#eb8f35', '#c48100', '#aa4801'],
+                [that['coefficient'][1]['staticCoefficient']]: ['#0173a1', '#2478dc', '#244ddc'],
+                [that['coefficient'][2]['staticCoefficient']]: ['#479600', '#038203', '#015c22']
+            }
+            //svg绘制参数
+        this.currentLineColor = '#FF0000';
+        this.currentLineWidth = 5 * this.fontSize / 24;
+        this.highlightLineColor = '#FF0000';
+        this.highlightLineWidth = 5 * this.fontSize / 24;
+        this.highlightPointRadius = 7.5 * this.fontSize / 24;
+        this.selectCircleColor = '#FFFC00';
+        this.selectedPointRadius = 12 * this.fontSize / 24;
 
-		return this;
-	}
+        this.MAX_LINE_NUM = 3;
 
-	destroy() {
-		AppConfig._inst = null;
-	}
+        this.PULL_MAX = 5; // 弹簧测力计最大拉力5N
+        this.WOOD_WEIGHT = 50; // 木块重量
+        this.IRON_WEIGHT = 100; // 砝码重量
+        this.g = 10; // g = 10N/KG
+        this.STATIC_TIME = 3; // 动画总时间
+        this.CONVERSION_TIME = 0.2; // 动画总时间
+        this.DYNAMIC_TIME = 5.8; // 动画总时间
+        this.TOTAL_TIME = 9; // 动画总时间
+        this.WEIGHT_MAX = 2;
+        this.INIT_STATE = 0; //动画未开始状态
+        this.RUN_STATE = 1; //运动状态
+        this.PAUSE_STATE = 2; //暂停状态
+        this.COMPLETE_STATE = 3; //动画结束状态
+        this.SINGLE_SPEED = 1; //一倍速度
+        this.DOUBLE_SPEED = 2; //二倍速度
+
+        let preloadImagesArr = [
+            this.basePath + 'resources/images/barbg.png',
+            this.basePath + 'resources/images/graph-bg.png',
+            this.basePath + 'resources/images/wood.png',
+            this.basePath + 'resources/images/celiji.png'
+        ];
+		this.promiseArr = [];
+
+		preloadImagesArr.map((url)=>{
+			this.promiseArr.push(new Promise((resolve, reject)=>{				
+				var img = new Image();
+				img.src = url;
+				img.onload = function(){
+					resolve(img);
+				}
+				img.onerror = function(err){
+					reject(err);
+				}
+
+			}));
+		}, this);
+
+        return this;
+    }
+
+    destroy() {
+        AppConfig._inst = null;
+    }
 
 }
 
